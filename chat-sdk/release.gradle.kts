51fb0e6bd580d172c9b7f91565534c6c1ab5dc06
Rajat Mittal <rajatttt@amazon.com>
2024-10-04 15:09:10 -0700

    Moved credentials to gradle.properties as local.properties does not exist on github

--- chat-sdk/release.gradle.kts
+++ chat-sdk/release.gradle.kts
@@ -10,21 +10,15 @@ val versionProperties = Properties().apply {
     load(file("$rootDir/chat-sdk/version.properties").inputStream())
 }
 
-// Load secrets from local.properties
-val localProperties = Properties().apply {
-    load(file("$rootDir/local.properties").inputStream())
-}
-
 val _sdkVersion = versionProperties.getProperty("sdkVersion")
 val _groupId = versionProperties.getProperty("groupId")
 val _artifactId = versionProperties.getProperty("artifactId")
 
-val _signingKeyRingFile = localProperties.getProperty("signing.secretKeyRingFile")
-val _signingPassword = localProperties.getProperty("signing.password")
-val _keyId = localProperties.getProperty("signing.keyId")
-val sonatypeUsername = localProperties.getProperty("SONATYPE_TOKEN_USERNAME")
-val sonatypePassword = localProperties.getProperty("SONATYPE_TOKEN_PASSWORD")
-
+val _signingKeyRingFile = findProperty("signing.secretKeyRingFile") as String?
+val _signingPassword = findProperty("signing.password") as String?
+val _keyId = findProperty("signing.keyId") as String?
+val sonatypeUsername = findProperty("SONATYPE_TOKEN_USERNAME") as String?
+val sonatypePassword = findProperty("SONATYPE_TOKEN_PASSWORD") as String?
 
 // Helper function to configure POM metadata
 fun MavenPublication.configurePom() {
@@ -57,51 +51,61 @@ fun MavenPublication.configurePom() {
     }
 }
 
-configure<PublishingExtension> {
-    publications {
-        create<MavenPublication>("publishChatSDK") {
-            groupId = _groupId
-            artifactId = _artifactId
-            version = _sdkVersion
+if (sonatypeUsername.isNullOrEmpty() || sonatypePassword.isNullOrEmpty()) {
+    logger.warn("Missing Sonatype credentials. Skipping publication...")
+} else {
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("publishChatSDK") {
+                groupId = _groupId
+                artifactId = _artifactId
+                version = _sdkVersion
 
-            // Ensure the AAR artifact is included
-            afterEvaluate {
-                artifact(tasks.getByName("bundleReleaseAar"))
+                // Ensure the AAR artifact is included
+                afterEvaluate {
+                    artifact(tasks.getByName("bundleReleaseAar"))
+                }
+
+                // Configure POM metadata using the helper function
+                configurePom()
             }
-
-            // Configure POM metadata using the helper function
-            configurePom()
         }
-    }
 
-    // Repository configuration for Sonatype
-    repositories {
-        maven {
-            credentials {
-                username = sonatypeUsername
-                password = sonatypePassword
+        // Repository configuration for Sonatype
+        repositories {
+            maven {
+                credentials {
+                    username = sonatypeUsername
+                    password = sonatypePassword
+                }
+                val releasesRepoUrl =
+                    uri("https://aws.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                val snapshotsRepoUrl =
+                    uri("https://aws.oss.sonatype.org/content/repositories/snapshots/")
+                url = if (_sdkVersion.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
             }
-            val releasesRepoUrl = uri("https://aws.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://aws.oss.sonatype.org/content/repositories/snapshots/")
-            url = if (_sdkVersion.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
         }
     }
 }
 
-configure<SigningExtension> {
-    // Load signing details from local.properties
-    val signingKey = _signingKeyRingFile
-    val signingPassword = _signingPassword
-    val keyId = _keyId
+if (_signingKeyRingFile.isNullOrEmpty() || _signingPassword.isNullOrEmpty() || _keyId.isNullOrEmpty()) {
+    logger.warn("Missing GPG signing properties. Skipping signing...")
+} else {
+    configure<SigningExtension> {
+        // Load signing details from local.properties
+        val signingKey = _signingKeyRingFile
+        val signingPassword = _signingPassword
+        val keyId = _keyId
 
-    // Ensure the required properties are available and use in-memory keys
-    if (signingKey != null && signingPassword != null && keyId != null) {
-        val privateKey = file(signingKey).readText()
-        useInMemoryPgpKeys(keyId, privateKey, signingPassword)
-    } else {
-        logger.error("Missing GPG Signing Properties.")
+        // Ensure the required properties are available and use in-memory keys
+        if (signingKey != null && signingPassword != null && keyId != null) {
+            val privateKey = file(signingKey).readText()
+            useInMemoryPgpKeys(keyId, privateKey, signingPassword)
+        } else {
+            logger.error("Missing GPG Signing Properties.")
+        }
+
+        // Sign the Maven publication
+        sign(extensions.getByType(PublishingExtension::class).publications["publishChatSDK"])
     }
-
-    // Sign the Maven publication
-    sign(extensions.getByType(PublishingExtension::class).publications["publishChatSDK"])
 }
