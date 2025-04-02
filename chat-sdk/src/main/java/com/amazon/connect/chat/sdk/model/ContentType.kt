45e100b0aedda69a6c38ac977d5af87cfeb5d9ea
Michael Liao <mikeliao@amazon.com>
2025-04-02 14:06:03 -0700

    Adding additional events to ContentType

--- chat-sdk/src/main/java/com/amazon/connect/chat/sdk/model/ContentType.kt
+++ chat-sdk/src/main/java/com/amazon/connect/chat/sdk/model/ContentType.kt
@@ -15,7 +15,23 @@ enum class ContentType(val type: String){
     PLAIN_TEXT("text/plain"),
     RICH_TEXT("text/markdown"),
     INTERACTIVE_TEXT("application/vnd.amazonaws.connect.message.interactive"),
-    INTERACTIVE_RESPONSE("application/vnd.amazonaws.connect.message.interactive.response");
+    INTERACTIVE_RESPONSE("application/vnd.amazonaws.connect.message.interactive.response"),
+    AUTHENTICATION_INITIATED("application/vnd.amazonaws.connect.event.authentication.initiated"),
+    AUTHENTICATION_SUCCESSFUL("application/vnd.amazonaws.connect.event.authentication.succeeded"),
+    AUTHENTICATION_FAILED("application/vnd.amazonaws.connect.event.authentication.failed"),
+    AUTHENTICATION_TIMEOUT("application/vnd.amazonaws.connect.event.authentication.timeout"),
+    AUTHENTICATION_EXPIRED("application/vnd.amazonaws.connect.event.authentication.expired"),
+    AUTHENTICATION_CANCELLED("application/vnd.amazonaws.connect.event.authentication.cancelled"),
+    PARTICIPANT_DISPLAY_NAME_UPDATED("application/vnd.amazonaws.connect.event.participant.displayname.updated"),
+    PARTICIPANT_ACTIVE("application/vnd.amazonaws.connect.event.participant.active"),
+    PARTICIPANT_INACTIVE("application/vnd.amazonaws.connect.event.participant.inactive"),
+    TRANSFER_SUCCEEDED("application/vnd.amazonaws.connect.event.transfer.succeeded"),
+    TRANSFER_FAILED("application/vnd.amazonaws.connect.event.transfer.failed"),
+    PARTICIPANT_IDLE("application/vnd.amazonaws.connect.event.participant.idle"),
+    PARTICIPANT_RETURNED("application/vnd.amazonaws.connect.event.participant.returned"),
+    PARTICIPANT_INVITED("application/vnd.amazonaws.connect.event.participant.invited"),
+    AUTO_DISCONNECTION("application/vnd.amazonaws.connect.event.participant.autodisconnection"),
+    CHAT_REHYDRATED("application/vnd.amazonaws.connect.event.chat.rehydrated");
 
     companion object {
         fun fromType(type: String): ContentType? {
