8c351a9c44d3b431480bc772a350bcce51dd164d
Michael Liao <mikeliao@amazon.com>
2025-08-04 09:48:39 -0700

    Removing config data

--- app/src/main/java/com/amazon/connect/chat/androidchatexample/Config.kt
+++ app/src/main/java/com/amazon/connect/chat/androidchatexample/Config.kt
@@ -3,9 +3,9 @@ package com.amazon.connect.chat.androidchatexample
 import com.amazonaws.regions.Regions
 
 object Config {
-    val connectInstanceId: String = "9e330fe8-389b-4e51-b6b2-c249462d7e33"
-    val contactFlowId: String = "a1acd643-869d-45a0-b758-d380214580cd"
-    val startChatEndpoint: String = "https://kopg30tz0m.execute-api.us-west-2.amazonaws.com/Prod/"
+    val connectInstanceId: String = ""
+    val contactFlowId: String = ""
+    val startChatEndpoint: String = "https://<endpoint>.execute-api.<region>.amazonaws.com/Prod/"
     val region: Regions = Regions.US_WEST_2
     val agentName = "AGENT"
     val customerName = "CUSTOMER"
